e66574a1d1c816ba1b78363fdac95a237f766548
ahsanjuly29 <ahsan.july29@gmail.com>
2025-08-31 02:26:17 +0600

    assets updated

--- CRUD-Ajax-Toastr/AdminLTE3/app.blade.php
+++ CRUD-Ajax-Toastr/AdminLTE3/app.blade.php
@@ -108,7 +108,8 @@
                     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                         <a href="#" class="dropdown-item">
                             <div class="media">
-                                <img src="img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                                <img src="{{ asset('assets/img/user1-128x128.jpg') }}" alt="User Avatar"
+                                    class="img-size-50 mr-3 img-circle">
                                 <div class="media-body">
                                     <h3 class="dropdown-item-title">
                                         Brad Diesel
@@ -125,7 +126,7 @@
                         <a href="#" class="dropdown-item">
                             <!-- Message Start -->
                             <div class="media">
-                                <img src="img/user8-128x128.jpg" alt="User Avatar"
+                                <img src="{{ asset('assets/img/user8-128x128.jpg') }}" alt="User Avatar"
                                     class="img-size-50 img-circle mr-3">
                                 <div class="media-body">
                                     <h3 class="dropdown-item-title">
@@ -141,7 +142,7 @@
                         <div class="dropdown-divider"></div>
                         <a href="#" class="dropdown-item">
                             <div class="media">
-                                <img src="img/user3-128x128.jpg" alt="User Avatar"
+                                <img src="{{ asset('assets/img/user3-128x128.jpg') }}" alt="User Avatar"
                                     class="img-size-50 img-circle mr-3">
                                 <div class="media-body">
                                     <h3 class="dropdown-item-title">
@@ -194,15 +195,16 @@
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <a href="../../index3.html" class="brand-link">
-                <img src="img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{ asset('assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">AdminLTE 3</span>
             </a>
 
             <div class="sidebar">
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ asset('assets/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                            alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">Alexander Pierce</a>
@@ -927,9 +929,6 @@
     <script src="{{ asset('assets/js/summernote-bs4.min.js') }}"></script>
 
     <script src="{{ asset('assets/js/jquery.overlayScrollbars.min.js') }}"></script>
-
-    <script src="{{ asset('assets/js/adminlte.min.js') }}"></script>
-
 
     {{-- Common ajax data for CRUD operation --}}
     <script src="{{ asset('assets/js/ajax-jquery-crud.js') }}"></script>
